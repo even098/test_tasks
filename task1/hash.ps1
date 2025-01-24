006781f805523ee86322803f151ab186af0bb995
even098 <143206114+even098@users.noreply.github.com>
2025-01-24 22:07:54 +0500

    Add files via upload

---
+++ task1/hash.ps1
@@ -0,0 +1,15 @@
+﻿$ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -eq 'Ethernet 2'}).IPAddress
+$response = Invoke-WebRequest -Uri "http://$ip/index.html" -Method GET
+$localHash = Get-FileHash "$env:USERPROFILE\test.zip" -Algorithm SHA256
+$regex = [regex] '<p>([a-fA-F0-9]+)</p>'
+if ($regex.Match($response.Content).Success) {
+    $webHash = $regex.Match($response.Content).Groups[1].Value
+}
+if ($webHash -ne $localHash.Hash) {
+    Stop-Process -Name "write.exe"
+    Remove-Item "$env:USERPROFILE\test.zip"
+    Remove-Item "$env:USERPROFILE\write.exe"
+    Remove-Item "$env:USERPROFILE\hash.ps1"
+    Invoke-WebRequest -Uri "http://$ip/test.zip" -OutFile "$env:USERPROFILE\test.zip"
+    Expand-Archive -Path "$env:USERPROFILE\test.zip" -DestinationPath "$env:USERPROFILE\"
+}
---
+++ task1/main.ps1
@@ -0,0 +1,6 @@
+﻿$ip = (Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.InterfaceAlias -eq 'Ethernet 2'}).IPAddress
+Invoke-WebRequest -Uri "http://$ip/test.zip" -OutFile "$env:USERPROFILE\test.zip"
+Expand-Archive -Path "$env:USERPROFILE\test.zip" -DestinationPath "$env:USERPROFILE\"
+$Action = New-ScheduledTaskAction -Execute "$env:USERPROFILE\write.exe; $env:USERPROFILE\hash.ps1"
+$Trigger = New-ScheduledTaskTrigger -At 12:00 -Daily
+Register-ScheduledTask -Action $Action -Trigger $Trigger -TaskName "TimeToWrite"
